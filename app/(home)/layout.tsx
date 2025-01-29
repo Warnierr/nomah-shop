95f5d92d51eef90bc5591e64884241ddcb941d08
Warnierr <rww.warnier@gmail.com>
2025-01-29 23:47:30 +0100

    Add HomeLayout component with Header and Footer

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/shared/header'
+import Footer from '@/public/components/shared/footer'
+
+export default function HomeLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className='flex min-h-screen flex-col'>
+      <Header />
+      <main className='flex-1 flex flex-col'>{children}</main>
+      <Footer />
+    </div>
+  )
+}
