48e56355b0e4febeeb61a32aa25a4d4918c83207
Warnierr <rww.warnier@gmail.com>
2025-02-21 23:41:29 +0100

    docs: add comments

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,16 +1,44 @@
-import Header from '@/components/shared/header'
-import Footer from '@/components/shared/footer'
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import { APP_DESCRIPTION, APP_NAME, APP_SLOGAN } from '@/lib/constants'
 
-export default async function HomeLayout({
+// Load the Geist Sans font with a CSS variable for styling
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+// Load the Geist Mono font with a CSS variable for styling
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+// Define metadata for the application
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${APP_NAME}`, // Dynamic page title template
+    default: `${APP_NAME}. ${APP_SLOGAN}`, // Default title when no specific title is provided
+  },
+  description: APP_DESCRIPTION, // Meta description for SEO
+}
+
+// Root layout component wrapping the entire application
+export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <Header />
-      <main className='flex-1 flex flex-col'>{children}</main>
-      <Footer />
-    </div>
+    <html lang='en'>
+      {' '}
+      {/* Set the default language to English */}
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`} // Apply font variables and enable anti-aliasing
+      >
+        {children} {/* Render the child components inside the body */}
+      </body>
+    </html>
   )
 }
